601ec7e6f52c4a46e94150b2b04594aaf0e4aa2e
parisa <mbkparisa@gmail.com>
2020-12-15 22:27:03 +0330

    my first website whit laravel

---
+++ public/Site/rd-navbar-master/documentation/sections/introduction/en/__description.php
@@ -0,0 +1,96 @@
+<h2 class="item1">Introduction</h2>
+
+<h5>
+    Full-function, powerful and flexible navigation is easier now.
+</h5>
+
+<h3>Where to start?</h3>
+
+<ul class="marked-list">
+    <li>
+        Click the following link to view the demonstration of this script work:
+        <a href='./index.php?lang=ru&section=demo'>Demo.</a>
+        .
+    </li>
+    <li>
+        The script itself is available in our public repository:
+        <a href='http://products.git.devoffice.com/coding-development/rd-navbar'>Git</a>
+    </li>
+</ul>
+
+<h3>
+    Supported browsers
+</h3>
+
+<p>
+    The script was tested on Windows, Linux and MacOS systems and supports the following browsers:
+</p>
+
+<ul class="inline-list">
+    <li class="center">
+        <img src="img/ie.png" alt=""/>
+
+        <div><b>9+</b></div>
+    </li>
+    <li class="center">
+        <img src="img/chrome.png" alt=""/>
+
+        <div><b>40+</b></div>
+    </li>
+    <li class="center">
+        <img src="img/firefox.png" alt=""/>
+
+        <div><b>35+</b></div>
+    </li>
+    <li class="center">
+        <img src="img/safari.png" alt=""/>
+
+        <div><b>8+</b></div>
+    </li>
+</ul>
+
+<p>
+    The test was not executed in other browsers. Write our author about all found bugs using the contact information provided below.
+</p>
+
+<h3>Requirements</h3>
+
+<p>
+    You need to satisfy the following requirements for the script work:
+</p>
+
+<ul class="marked-list">
+    <li>
+        jQuery 1.11+
+    </li>
+    <li>
+        jQuery Easing 1.1+ (For One Page navigation)
+    </li>
+</ul>
+
+<h3>
+    About the author
+</h3>
+
+<p>
+    <strong>Gusarov Yevgeniy (Diversant | StMechanus)</strong>
+</p>
+
+<dl class="inline-term">
+    <dt>E-mail</dt>
+    <dd>stmechanus@gmail.com</dd>
+</dl>
+<dl class="inline-term">
+    <dt>Skype</dt>
+    <dd>stmechanus@gmail.com</dd>
+</dl>
+<dl class="inline-term">
+    <dt>Bitrix</dt>
+    <dd>Diversant</dd>
+</dl>
+<dl class="inline-term">
+    <dt>LinkedIn</dt>
+    <dd>
+        <a href="https://ua.linkedin.com/pub/evgeniy-gusarov/8a/a40/54a">Gusarov Yevgeniy</a>
+    </dd>
+</dl>
